e69d855d2a6729dedf55e274883254051890d516
luizpbello <luizpbello2@gmail.com>
2023-06-13 19:59:34 -0300

    feat: criando as migrations

--- app/Http/Controllers/Forum/CommentsController.php
+++ app/Http/Controllers/Forum/CommentsController.php
@@ -1,11 +1,30 @@
 <?php
 
 namespace App\Http\Controllers\Forum;
-
+use App\Models\Comments;
+use App\Models\Topic;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class CommentsController extends Controller
 {
+    public function store(Request $request, Topic $topic){
+        $validateData = $request -> validate([
+            'content' => 'required|string'
+        ]);
+        $comment = $topic -> comments() ->create($validateData);
+
+        return response()->json(['message', 'Comentário enviado com sucesso', 'comment' => $comment]);
+    }
+
+
+    public function index(Topic $topic){
+        $comments = $topic->comment()->get();
+        return response()->json(['comments', $comments]);
+    }
+
+    public function show(Comments $comment){
+        return response()->json(['comment', $comment]);
+    }
     
 }
