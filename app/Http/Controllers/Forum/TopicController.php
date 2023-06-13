3f64437e592f527a1c090e6a3e53a6cbca24cefa
luizpbello <luizpbello2@gmail.com>
2023-06-13 19:19:51 -0300

    feat: criando topicController

--- app/Http/Controllers/Forum/TopicController.php
+++ app/Http/Controllers/Forum/TopicController.php
@@ -8,6 +8,49 @@ use App\Models\Topic;
 
 class TopicController extends Controller
 {
-  
+    public function store(Request $request){
+        $validateData = $request -> validate([
+            'title'=>'required|string',
+            'content'=>'required|string',
+            'cateogry'=>'required|string'
+        ]);
+    
+        $topic = Topic::create($validateData);
+    
+        return response() ->json(['message','Tópico criado com sucesso', 'topic' => $topic ]);
+    
+      }
+    
+    
+      public function update(Request $request, Topic $topic){
+        $validateData = $request -> validate([
+            'title'=>'required|string',
+            'content'=>'required|string',
+            'cateogry'=>'required|string'
+        ]);
+    
+        $topic -> update($validateData);
+    
+        return response() ->json(['message' => 'Tópico atualizado com succeso', 'topic' => $topic]);
+      }
+    
+    
+      public function destroy(Topic $topic){
+        $topic -> delete();
+    
+        return response() -> json(['message' => 'Tópico excluido com sucesso']);
+      }
+    
+    
+      public function index(){
+        $topics = Topic::all();
+    
+        return response() -> json(['topics' => $topics]);
+      }
+    
+    
+      public function show(Topic $topic){
+        return response() -> json(['topic', $topic]);
+      }
     
 }
